2809a44baecd00e0abc0432df97421cbf8d741b1
Delgadomelina <delgadomelinanicoles@gmail.com>
2025-10-28 14:58:23 -0300

    logos
    no

---
+++ Login/frontend/src/profeciones.jsx
@@ -0,0 +1,97 @@
+
+import "/profeciones.css";
+import { Link } from "react-router-dom";
+
+function Inicio() {
+
+    return(
+
+
+        p lxpp p5<section>
+            <div class="areas">
+                <> Medicina general <br> <img src="imag/MedicinaG.jpg" alt=""> </>
+            </div>
+
+
+            <div class="areas">
+                <>Medico cirujano <br> <img src="imag/medicoc.jpg" alt="" class="imag"> </>
+            </div>
+
+
+            <div class="areas">
+                <> Nutrición<br> <img src="imag/nutricion.jpg" alt="" class="imag"> </>
+            </div>
+
+        </section>
+
+        <section class="areas1">
+
+            <div class="areas">
+                <> Odontología <br> <img src="imag/odontologia.jpg" alt="" class="imag"> </>
+            </div>
+
+
+            <div class="areas">
+                <>Oftangologia <br> <img src="imag/oftangologia.jpg" alt="" class="imag"> </>
+            </div>
+
+
+            <div class="areas">
+                <> pediatria<br> <img src="imag/pediatria.jpg" alt="" class="imag"> </>
+            </div>
+
+        </section>
+
+        <section class="areas1">
+
+            <div class="areas">
+                <> Dermatología <br> <img src="imag/Dermatología.jpg" alt="" class="imag"> </>
+            </div>
+
+
+            <div class="areas">
+                <>Gastrología <br> <img src="imag/Gastrología.jpg" alt="" class="imag"> </>
+            </div>
+
+
+            <div class="areas">
+                <> Neumología<br> <img src="imag/Neumología.jpg" alt="" class="imag"> </>
+            </div>
+
+
+ <footer class="footer">
+   
+  <div class="footer-container">
+    <div class="footer-section">
+      <h3>Sanatorio del Norte</h3>
+      <p>Comprometidos con tu salud y bienestar.</p>
+    </div>
+
+    <div class="footer-section">
+      <h4>Enlaces útiles</h4>
+      <ul>
+        <li>Acerca de</li>
+        <li>Servicios</li>
+        <li>Política de Privacidad</li>
+      </ul>
+    </div>
+
+    <div class="footer-section">
+      <h4>Redes sociales</h4>
+      <div class="social-icons">
+        <i class="fab fa-instagram"><img src="imag/instagram-logo.png" alt=""></i>
+        <i class="fab fa-facebook"><img src="imag/facebook.png" alt=""></i>
+        <i class="fab fa-whatsapp"><img src="imag/" alt=""></i>
+        <i class="fab fa-telegram"><img src="imag/telegram.png" alt=""></i>
+      </div>
+    </div>
+  </div>
+
+  <div class="footer-bottom">
+    © 2025 Sanatorio del Norte. Todos los derechos reservados.
+  </div>
+ 
+ </footer>
+
+    )
+}

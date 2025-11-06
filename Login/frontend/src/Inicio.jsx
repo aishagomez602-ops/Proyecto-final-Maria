30f902f58034bb9b6ac93bc5890cfbb31b801fe9
Delgadomelina <delgadomelinanicoles@gmail.com>
2025-11-06 15:37:05 -0300

    icon
    de whatsApp

--- Login/frontend/src/Inicio.jsx
+++ Login/frontend/src/Inicio.jsx
@@ -1,6 +1,6 @@
 
 import "./Inicio.css";
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";  
 
 
 
@@ -63,7 +63,7 @@ function Inicio () {
                 <img src="imag/facebook.png" alt="Facebook" />
               </i>
               <i className="fab fa-whatsapp">
-                <img src="imag/image.png" alt="WhatsApp" />
+                <img src="imag/whatsapp.png" alt="WhatsApp" />
               </i>
               <i className="fab fa-telegram">
                 <img src="imag/telegram.png" alt="Telegram" />
